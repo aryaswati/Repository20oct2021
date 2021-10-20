b1d228abde7947a65822dc960cfa28d2261511b7
aryaswati <92774626+aryaswati@users.noreply.github.com>
2021-10-20 15:02:53 +0530

    Add files via upload

--- post_1.js
+++ post_1.js
@@ -2,8 +2,7 @@ const express = require('express');
 const app = express();
 //const bodyparser = require("body-parser");
 //app.use(bodyparser.json());
-app.use(express.json());
-app.use(express.urlencoded ({extended: false}));
+
 const users = [
     {Name: 'Madhav', Age: 18},
     {Name: 'Nayan' , Age:20},
@@ -15,27 +14,14 @@ const posts =[
     {Title:'My favourite food'},
     {Title:'My favourite place'}
 ];
-app.listen(9000,()=>console.log('express server is running at port no.: 9000'));
 app.get('/', (req,res) => {
-    res.send ({
+    req.send ({
         msg:'Hello',
         user:{}
     });
 });
 app.post('/', (req, res) => {
-    //console.log (req.body);
-    const user = req.body;
-    users.push(user);
+    console.log (req.body);
     res.status (201).send('created user');
-
-});
-
-app.post('/posts', (req, res) => {
-    console.log (req.headers);
-    
-});
-app.get('/posts', (req, res) => {
-    console.log (req.query);
-   
 
 });
